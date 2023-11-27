8494594608931e0ff0ed45c6d7cba177d7fb3359
Molnár Tamás <molnarj@eotvos2.local>
2023-11-27 09:16:05 +0100

    1

--- 10C1/231127/tanuloAdatok.py
+++ 10C1/231127/tanuloAdatok.py
@@ -1,6 +1,6 @@
 import os
 os.system("cls")
-
+'''
 tanulok = [
     {'vnev':'Kovács',
      'knev':'Béla',
@@ -8,6 +8,21 @@ tanulok = [
     {'vnev':'Molnár',
      'knev':'Márton',
      'atlag':2.8}
-]
+]'''
+
+tanulok = []
+
+while True:
+    vnev = input("Add meg a vezetéknevet: ")
+    if vnev == "":
+        break
+    knev = input("Add meg a keresztnevet")
+    atlag = input("Add meg az átlagot: ")
+    atlag = float(atlag)
+    tanulo = {}
+    tanulo['vnev'] = vnev
+    tanulo['knev'] = knev
+    tanulo['atlag'] = atlag
+    tanulok.append(tanulo)
 
 print(tanulok)
